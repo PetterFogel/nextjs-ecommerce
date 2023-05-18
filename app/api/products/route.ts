39c05c83345258e3e83983502122b94326f1ce51
PetterFogel <petterfogel96@gmail.com>
2023-05-18 14:39:15 +0200

    Implement post request to add product to db

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,8 +1,9 @@
 import Product from "@/models/product";
-import { connectToDB } from "@/utils/database";
-import { NextResponse } from "next/server";
+import { connectToDB } from "@/lib/database";
+import { NextRequest, NextResponse } from "next/server";
+import { IProduct } from "@/types/product";
 
-export const GET = async (_: Request) => {
+export const GET = async (_: NextRequest) => {
   try {
     await connectToDB();
 
@@ -11,5 +12,20 @@ export const GET = async (_: Request) => {
     return new NextResponse(JSON.stringify(products), { status: 200 });
   } catch (error) {
     return new NextResponse("Failed to fetch all products", { status: 500 });
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  const values = await req.json();
+  try {
+    await connectToDB();
+    const newpProduct = new Product(values as IProduct);
+
+    await newpProduct.save();
+    return new NextResponse("Product was created successfully!", { status: 201 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return new NextResponse(error.message, { status: 400 });
+    }
   }
 };
