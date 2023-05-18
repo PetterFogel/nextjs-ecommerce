0052d9742c08f5c9f886e43a4254741f8b01082f
PetterFogel <petterfogel96@gmail.com>
2023-05-18 14:32:35 +0200

    Moved to current folder

--- utils/fetchData.ts
+++ utils/fetchData.ts
@@ -3,7 +3,7 @@ export const fetchData = async (path: string, id?: string) => {
     const url = `${process.env.REACT_APP_API_BASEURL}/api/${path}/${id || ""}`;
     const res = await fetch(url, {
       next: {
-        revalidate: 5
+        revalidate: 30
       }
     });
 
