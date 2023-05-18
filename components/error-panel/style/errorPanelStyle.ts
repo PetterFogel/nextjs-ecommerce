5d87487ec4d3e44b6247488a84f916e563514092
PetterFogel <petterfogel96@gmail.com>
2023-05-18 14:33:08 +0200

    Implement new maxWidth style

--- components/error-panel/style/errorPanelStyle.ts
+++ components/error-panel/style/errorPanelStyle.ts
@@ -8,9 +8,10 @@ export const errorPanelStyles = makeStyles()((theme: Theme) => ({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    padding: theme.spacing(1),
+    maxWidth: "900px",
     [theme.breakpoints.down("md")]: {
-      width: "100%",
-      padding: theme.spacing(1)
+      width: "100%"
     }
   },
   errorPanel: {
