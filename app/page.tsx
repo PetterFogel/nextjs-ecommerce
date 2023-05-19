ca588c620296e4e6a7c1ede3f001aa7665de8627
PetterFogel <petterfogel96@gmail.com>
2023-05-19 09:47:09 +0200

    Implement new content and styling for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from "next";
+import "./globals.css";
 
 const Home: NextPage = () => {
   return (
     <main>
-      <h2>Home Page</h2>
+      <div className="heroSection"></div>
+      <div className="introContainer">
+        <h1 className="title">
+          Welcome to the The Many Saints, quality clothes to match your style
+        </h1>
+      </div>
     </main>
   );
 };
