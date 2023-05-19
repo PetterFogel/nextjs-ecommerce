ae0a7b01631c3e37870b5db7a4696f7ebe79e963
PetterFogel <petterfogel96@gmail.com>
2023-05-19 10:58:18 +0200

    Test to fix vercel bug

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,29 +1,8 @@
-import { IProduct } from "@/types/product";
 import { fetchData } from "@/utils/fetchData";
 import { ProductDetails } from "../ProductDetails";
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-export async function generateMetadata({ params }: Props) {
-  const product = await fetchData("products", params.id);
-
-  if (!product) return {};
-
-  return {
-    title: product.title
-  };
-}
-
-export async function generateStaticParams() {
-  const products: IProduct[] = await fetchData("products");
-
-  return products.map((p) => ({
-    id: p._id
-  }));
+interface Props {
+  params: { id: string };
 }
 
 const ProductItem = async ({ params }: Props) => {
