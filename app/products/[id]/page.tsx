b84d5669b72f5deae09fa47f7e9a20c771528e50
PetterFogel <petterfogel96@gmail.com>
2023-05-19 15:43:55 +0200

    Test to fix vercel bug

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -6,18 +6,15 @@ import { Suspense } from "react";
 export async function generateStaticParams() {
   const productsData: IProduct[] = await fetchData("products");
 
-  const ids = productsData.map((p) => {
-    return {
-      params: {
-        id: p._id
-      }
-    };
-  });
+  const ids = productsData.map((p) => ({
+    id: p._id
+  }));
 
-  return ids;
+  return ids.map((id) => id.id);
 }
 
 async function ProductItem({ params }: { params: { id: string } }) {
+  console.log(params.id);
   const product = await fetchData("products", params.id);
   if (!product) return;
   return (
