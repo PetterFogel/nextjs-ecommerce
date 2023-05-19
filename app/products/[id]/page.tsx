ce6e93b444a85995118edcc319f7bbb0aa0edacf
PetterFogel <petterfogel96@gmail.com>
2023-05-19 12:53:02 +0200

    Test to fix vercel bug

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,8 @@
 import { IProduct } from "@/types/product";
+import axios from "axios";
 
 async function fetchProduct(id: string): Promise<IProduct> {
-  const response = await fetch(
+  const response = await axios.get(
     `${process.env.API_BASE_URL}/api/products/${id}`,
     {
       headers: {
@@ -9,7 +10,7 @@ async function fetchProduct(id: string): Promise<IProduct> {
       }
     }
   );
-  const contents = await response.json();
+  const contents = response.data;
   return contents;
 }
 
