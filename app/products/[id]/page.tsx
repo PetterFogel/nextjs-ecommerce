cb94bfbdd5763155f22b305feb2c801b13063d5f
PetterFogel <petterfogel96@gmail.com>
2023-05-19 20:10:11 +0200

    Test to fix vercel bug

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,8 +1,12 @@
 import { ProductDetails } from "@/common/components/ProductDetails";
-import { fetchData } from "@/utils/fetchData";
+
+async function getProduct(id: string) {
+  const res = await fetch(`${process.env.API_BASE_URL}/api/products/${id}`);
+  return res.json();
+}
 
 const ProductItem = async ({ params }: { params: { id: string } }) => {
-  const product = await fetchData("products", params.id);
+  const product = await getProduct(params.id);
   if (!product) return;
   return <ProductDetails product={product} />;
 };
