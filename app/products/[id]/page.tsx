3dd80f08f569fad29383095d79339d3fa550914a
PetterFogel <petterfogel96@gmail.com>
2023-05-20 09:04:21 +0200

    Implement logic to statically generate routes

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,20 @@
 import { fetchData } from "@/utils/fetchData";
 import { ProductDetails } from "@/common/components/ProductDetails";
+import { IProduct } from "@/types/product";
 
-const ProductItem = async ({ params }: { params: { id: string } }) => {
+interface Props {
+  params: { id: string };
+}
+
+export const generateStaticParams = async () => {
+  const products: IProduct[] = await fetchData("products");
+
+  return products.map((p) => ({
+    id: p._id
+  }));
+};
+
+const ProductItem = async ({ params }: Props) => {
   const product = await fetchData(params.id);
   if (!product) return;
   return <ProductDetails product={product} />;
