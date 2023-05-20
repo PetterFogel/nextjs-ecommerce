2a0c115819944ea106519ed033661a5c218abb8f
PetterFogel <petterfogel96@gmail.com>
2023-05-20 08:58:33 +0200

    Implement logic to fetch specific product

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,12 +1,8 @@
+import { fetchData } from "@/utils/fetchData";
 import { ProductDetails } from "@/common/components/ProductDetails";
 
-async function getProduct(id: string) {
-  const res = await fetch(`${process.env.API_BASE_URL}/api/products/${id}`);
-  return res.json();
-}
-
 const ProductItem = async ({ params }: { params: { id: string } }) => {
-  const product = await getProduct(params.id);
+  const product = await fetchData(params.id);
   if (!product) return;
   return <ProductDetails product={product} />;
 };
