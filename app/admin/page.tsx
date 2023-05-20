a3cc9fa40a5491812915b139cb92732a311b3682
PetterFogel <petterfogel96@gmail.com>
2023-05-20 10:17:58 +0200

    Implement logic to fetch and render data + handle loading and error

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,19 @@
-import { NextPage } from "next";
+import { IProduct } from "@/types/product";
+import { fetchData } from "@/utils/fetchData";
 
-const AdminPage: NextPage = () => {
+const AdminPage = async () => {
+  const products = await fetchData("products");
+
   return (
     <main>
       <h2>Admin</h2>
+      <div>
+        {products.map((p: IProduct) => (
+          <div key={p._id}>
+            <div>{p.title}</div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
