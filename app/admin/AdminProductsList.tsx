a37b887da34d21b1f2843d4181265f4ea2516e0b
PetterFogel <petterfogel96@gmail.com>
2023-05-21 07:37:40 +0200

    Implement logic to fetch specific product

--- app/admin/AdminProductsList.tsx
+++ app/admin/AdminProductsList.tsx
@@ -8,19 +8,42 @@ import {
   TableHead,
   TableRow
 } from "@mui/material";
-import { FC } from "react";
 import { IProduct } from "@/types/product";
-import { AdminProductsItem } from "./AdminProductsItem";
+import { FC, useState } from "react";
+import { ProductDialog } from "./AdminProductDialog";
 import { adminPageStyles } from "./style/adminPageStyles";
+import { AdminFilterPanel } from "./AdminsFilterPanel";
+import { AdminProductsItem } from "./AdminProductsItem";
 
 interface Props {
   products: IProduct[];
 }
 
+const fetchProduct = async (productId: string | undefined) => {
+  if (!productId) return undefined;
+  const response = await fetch(`/api/products/${productId}`);
+  return await response.json();
+};
+
 export const AdminProductsList: FC<Props> = ({ products }) => {
   const { classes } = adminPageStyles();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [product, setProduct] = useState<IProduct | undefined>(undefined);
+
+  const openDialogHandler = async (productId?: string) => {
+    const product = await fetchProduct(productId);
+    setProduct(product);
+    setIsDialogOpen(true);
+  };
+
+  const closeDialogHandler = () => {
+    setProduct(undefined);
+    setIsDialogOpen(false);
+  };
+
   return (
     <main className={classes.root}>
+      <AdminFilterPanel onDialogOpenClick={openDialogHandler} />
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 900 }} aria-label="simple table">
           <TableHead>
@@ -35,11 +58,22 @@ export const AdminProductsList: FC<Props> = ({ products }) => {
           </TableHead>
           <TableBody>
             {products.map((product) => (
-              <AdminProductsItem key={product._id} product={product} />
+              <AdminProductsItem
+                key={product._id}
+                product={product}
+                onDialogOpenClick={openDialogHandler}
+              />
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      {isDialogOpen && (
+        <ProductDialog
+          product={product}
+          isDialogOpen={isDialogOpen}
+          onDialogCloseClick={closeDialogHandler}
+        />
+      )}
     </main>
   );
 };
