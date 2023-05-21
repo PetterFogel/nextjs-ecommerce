2c3f8aef2b689022b23d8b1ce411b0bb1bfa2ba9
PetterFogel <petterfogel96@gmail.com>
2023-05-21 07:35:47 +0200

    Implement some relevant constants

---
+++ common/constants/productState.ts
@@ -0,0 +1,12 @@
+import { IProduct } from "@/types/product";
+
+export const productState: IProduct = {
+  _id: "",
+  title: "",
+  category: "",
+  info: "",
+  price: 0,
+  imageUrl: "",
+  rating: 0,
+  sizes: []
+};
