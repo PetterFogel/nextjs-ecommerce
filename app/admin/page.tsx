e1ed8cb116dc9b7a7dd00cf57c4005733b208178
PetterFogel <petterfogel96@gmail.com>
2023-05-28 12:36:19 +0200

    Add notification alert to render in page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,18 @@
 "use client";
-
-import { useFetchProductsQuery } from "@/redux/api/productsApi";
 import { AdminProductsList } from "./AdminProductsList";
+import { NotificationAlert } from "@/common/components/notification-alert/NotificationAlert";
+import { useFetchProductsQuery } from "@/redux/api/productsApi";
+import "react-toastify/dist/ReactToastify.css";
 
 const AdminPage = async () => {
   const { data: products } = useFetchProductsQuery();
   if (!products) return;
-  return <AdminProductsList products={products} />;
+  return (
+    <>
+      <NotificationAlert />
+      <AdminProductsList products={products} />
+    </>
+  );
 };
 
 export default AdminPage;
