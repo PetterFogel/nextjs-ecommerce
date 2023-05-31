961d06afc5c48cae5dd2f91e505d8209e79d9635
PetterFogel <petterfogel96@gmail.com>
2023-05-31 14:25:19 +0200

    Implement signIn callback to create user

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import { connectToDB } from "@/lib/database";
+import User from "@/models/user";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -7,7 +9,24 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_ID || "",
       clientSecret: process.env.GOOGLE_SECRET || ""
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await connectToDB();
+        const { email, image, name } = user;
+        const userExist = await User.findOne({ email });
+
+        if (!userExist)
+          await User.create({ email, name, image, role: "CUSTOMER" });
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
+  }
 });
 
 export { handler as GET, handler as POST };
