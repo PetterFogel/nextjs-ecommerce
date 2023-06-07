00079757e87c04fa30b63db6c2e48742d8b5713e
PetterFogel <petterfogel96@gmail.com>
2023-06-07 12:28:28 +0200

    Implement logic to notify user of empty cart + total amount

--- app/checkout/CartList.tsx
+++ app/checkout/CartList.tsx
@@ -4,17 +4,15 @@ import { Divider } from "@mui/material";
 import { CartItem } from "./CartItem";
 import { useAppSelector } from "@/redux/hooks";
 import { checkoutSelector } from "@/redux/slices/checkout/cartSlice";
-import { checkoutPageStyles } from "./style/checkoutPageStyles";
 
 export const CartList: FC = () => {
-  const { classes } = checkoutPageStyles();
   const { cartItems } = useAppSelector(checkoutSelector);
   return (
-    <div className={classes.root}>
+    <>
       <Divider />
       {cartItems.map((item) => (
         <CartItem key={item._id} cartProduct={item} />
       ))}
-    </div>
+    </>
   );
 };
--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,10 +1,42 @@
+"use client";
 import { NextPage } from "next";
 import { CartList } from "./CartList";
+import { useRouter } from "next/navigation";
+import { useAppSelector } from "@/redux/hooks";
+import { checkoutSelector } from "@/redux/slices/checkout/cartSlice";
+import { Button, Typography } from "@mui/material";
+import { checkoutPageStyles } from "./style/checkoutPageStyles";
 
 const CheckoutPage: NextPage = () => {
+  const router = useRouter();
+  const { classes } = checkoutPageStyles();
+  const { cartItems, totalAmount } = useAppSelector(checkoutSelector);
+
   return (
-    <main>
-      <CartList />
+    <main className={classes.root}>
+      {cartItems.length === 0 ? (
+        <>
+          <Typography variant={"h3"} mb={2}>
+            Your cart is empty.
+          </Typography>
+          <Button
+            color="info"
+            variant="contained"
+            onClick={() => router.push("/products")}>
+            Continue Shopping
+          </Button>
+        </>
+      ) : (
+        <>
+          <CartList />
+          <div className={classes.priceInfoContainer}>
+            <Typography variant={"h3"} fontWeight={700}>
+              Total
+            </Typography>
+            <Typography variant={"h3"}>SEK: {totalAmount}</Typography>
+          </div>
+        </>
+      )}
     </main>
   );
 };
