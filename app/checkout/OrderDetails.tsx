d31678efe162cbf41a56f1615b6ae529cd79aaad
PetterFogel <petterfogel96@gmail.com>
2023-06-11 08:33:12 +0200

    Add personal information component with simple content

--- app/checkout/OrderDetails.tsx
+++ app/checkout/OrderDetails.tsx
@@ -1,10 +1,12 @@
 import { Typography } from "@mui/material";
 import { FC } from "react";
+import { PersonalInformation } from "./PersonalInformation";
 
 export const OrderDetails: FC = () => {
   return (
     <>
       <Typography variant="h3">Order Details</Typography>
+      <PersonalInformation />
     </>
   );
 };
---
+++ app/checkout/PersonalInformation.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Typography } from "@mui/material";
+
+export const PersonalInformation: FC = () => {
+  return (
+    <>
+      <Typography variant="h3">Your Information</Typography>
+    </>
+  );
+};
