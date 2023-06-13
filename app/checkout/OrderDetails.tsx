218709b33d04146936519861aeb1643af69a37fd
PetterFogel <petterfogel96@gmail.com>
2023-06-13 14:33:20 +0200

    Implement shipping component with simple content

--- app/checkout/OrderDetails.tsx
+++ app/checkout/OrderDetails.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { FC } from "react";
 import { PersonalInformation } from "./PersonalInformation";
+import { ShippingSelectionPanel } from "./ShippingSelectionPanel";
 
 export const OrderDetails: FC = () => {
   return (
     <>
+      <ShippingSelectionPanel />
       <PersonalInformation />
     </>
   );
