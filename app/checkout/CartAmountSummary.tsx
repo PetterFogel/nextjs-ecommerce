ae8498b551022ccae3f6feef709c5fe17fdded00
PetterFogel <petterfogel96@gmail.com>
2023-06-20 18:36:36 +0200

    Implement vat calculation of total amount

--- app/checkout/CartAmountSummary.tsx
+++ app/checkout/CartAmountSummary.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { FC } from "react";
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { checkoutPageStyles } from "./style/checkoutPageStyles";
 
 interface Props {
@@ -10,10 +10,18 @@ interface Props {
 export const CartAmountSummary: FC<Props> = ({ cartAmount }) => {
   const { classes } = checkoutPageStyles();
 
+  const VAT = Math.round(cartAmount * 0.2);
+
   return (
-    <div className={classes.spaceBetween}>
-      <Typography variant={"h3"}>Total</Typography>
-      <Typography variant={"h3"}>SEK: {cartAmount}</Typography>
-    </div>
+    <Stack spacing={1} mt={5} mb={5}>
+      <div className={classes.spaceBetween}>
+        <Typography variant={"subtitle1"}>Total</Typography>
+        <Typography variant={"subtitle1"}>{cartAmount} SEK</Typography>
+      </div>
+      <div className={classes.spaceBetween}>
+        <Typography variant={"subtitle1"}>VAT</Typography>
+        <Typography variant={"subtitle1"}>{VAT} SEK</Typography>
+      </div>
+    </Stack>
   );
 };
